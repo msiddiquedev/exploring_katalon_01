e6ff9f92ccdfda6b16899acc3c1ea066c042d143
msiddiquedev <mahtab.siddique.dev@gmail.com>
2024-09-10 00:48:13 +0530

    Test suits and reports.

---
+++ Test Suites/Login_TS.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Login_TS</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>7ea38fb4-acc2-4e0c-8db6-18c8b1f759eb</testSuiteGuid>
+   <testCaseLink>
+      <guid>be91e551-eb3b-497f-9c1c-97adb93d78ba</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Logion_TS</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
